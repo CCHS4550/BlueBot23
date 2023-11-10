d44857fc1d37445c2b0aa2ce1a71f958f6adab47
trainguy5455 <60554548+CCHS4550@users.noreply.github.com>
2023-11-09 17:14:03 -0700

    cbqcuob

--- BlueBot23/src/main/java/frc/robot/subsystems/Uppsies.java
+++ BlueBot23/src/main/java/frc/robot/subsystems/Uppsies.java
@@ -1,11 +1,28 @@
+import edu.wpi.first.wpilibj.Controller;
+import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 public class Uppsies extends SubsystemBase{
     
-    private final CCsparkMax elevator = new CCsparkMax("Elevator Moter", "em", RobotMap.ELEVATOR, MotorType.kBrushless, IdleMode.kBrake, RobotMap.ELEVATOR_REVERSE, RobotMap.DRIVE_ENCODER);
+    private final CCSparkMax elevator = new CCSparkMax("Elevator Moter", "em", RobotMap.ELEVATOR, MotorType.kBrushless, IdleMode.kBrake, RobotMap.ELEVATOR_REVERSE, RobotMap.DRIVE_ENCODER);
 
     public void INeedUppsies(double speed){
         elevator.set(speed);
 
     } // leedleleedleleeedlele - Patrick Star
+    public RunCommand elevatorUppies(double target) {
+        PidController controllerUppsies = new PidController(.5,0,0);
+        return new RunCommand(() -> {
+            controllerUppsies.calculate(elevator.getPosition(), target);
+            INeedUppsies();
+        }, this) {
+            @Override
+            public boolean isFinished() {
+                return false;
+            }
+        };
+    }
 
     
 }
--- BlueBot23/src/main/java/frc/robot/subsystems/Vroom.java
+++ BlueBot23/src/main/java/frc/robot/subsystems/Vroom.java
@@ -1,12 +1,13 @@
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 public class Vroom extends SubsystemBase {
-    private final CCsparkMax frontLeft = new CCsparkMax("Front Left", "fl", RobotMap.FRONT_LEFT, MotorType.kBrushless, IdleMode.kBrake, RobotMap.FRONT_LEFT_REVERSE, RobotMap.DRIVE_ENCODER);
-    private final CCsparkMax frontRight = new CCsparkMax("Front Right", "fr", RobotMap.FRONT_RIGHT, MotorType.kBrushless, IdleMode.kBrake, RobotMap.FRONT_RIGHT_REVERSE, RobotMap.DRIVE_ENCODER);
-    private final CCsparkMax backLeft = new CCsparkMax("Back Left", "bl", RobotMap.BACK_LEFT, MotorType.kBrushless, IdleMode.kBrake, RobotMap.BACK_LEFT_REVERSE, RobotMap.DRIVE_ENCODER);
-    private final CCsparkMax backRight = new CCsparkMax("Back Right", "br", RobotMap.BACK_RIGHT, MotorType.kBrushless, IdleMode.kBrake, RobotMap.BACK_RIGHT_REVERSE, RobotMap.DRIVE_ENCODER);
+    private final CCSparkMax frontLeft = new CCSparkMax("Front Left", "fl", RobotMap.FRONT_LEFT, MotorType.kBrushless, IdleMode.kBrake, RobotMap.FRONT_LEFT_REVERSE, RobotMap.DRIVE_ENCODER);
+    private final CCSparkMax frontRight = new CCSparkMax("Front Right", "fr", RobotMap.FRONT_RIGHT, MotorType.kBrushless, IdleMode.kBrake, RobotMap.FRONT_RIGHT_REVERSE, RobotMap.DRIVE_ENCODER);
+    private final CCSparkMax backLeft = new CCSparkMax("Back Left", "bl", RobotMap.BACK_LEFT, MotorType.kBrushless, IdleMode.kBrake, RobotMap.BACK_LEFT_REVERSE, RobotMap.DRIVE_ENCODER);
+    private final CCSparkMax backRight = new CCSparkMax("Back Right", "br", RobotMap.BACK_RIGHT, MotorType.kBrushless, IdleMode.kBrake, RobotMap.BACK_RIGHT_REVERSE, RobotMap.DRIVE_ENCODER);
 
-    MotorControllerGroup left = new MotorControllerGroup(frontleft, backleft);
-    MotorControllerGroup right = new MotorControllerGroup(frontright, backright);
+    MotorControllerGroup left = new MotorControllerGroup(frontLeft, backleft);
+    MotorControllerGroup right = new MotorControllerGroup(frontRight, backright);
 
     DifferentialDrive difDrive = new DifferentialDrive(left, right);
 
